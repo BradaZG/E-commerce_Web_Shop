208573eb3959113f889c803ffa6f7c0822d3845f
Domagoj Marusic <bradazg@gmail.com>
2020-11-29 18:42:44 +0100

    Connected Commerce.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 // import Navbar from './components/Navbar/Navbar';
 // import Products from './components/Products/Products';
 
 import { Products, Navbar } from './components';
+import { commerce } from './lib/commerce';
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
+  const fetchProducts = async () => {
+    const response = await commerce.products.list();
+    const data = await response.data;
+    //const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
+
+  const fetchCart = async () => {
+    // const response = await commerce.cart.retrieve();
+    // const cart = await commerce.cart.retrieve();
+    // setCart(cart)
+    setCart(await commerce.cart.retrieve());
+  };
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
+
+  console.log('cart', cart);
+
   return (
     <div>
-      <Navbar />
-      <Products />
+      <Navbar totalItems={cart.total_items} />
+      <Products products={products} onAddToCart={handleAddToCart} />
     </div>
   );
 };
--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -11,35 +11,40 @@ import { AddShoppingCart } from '@material-ui/icons';
 
 import useStyles from './styles';
 
-const Product = ({ product }) => {
+const Product = ({ product, onAddToCart }) => {
   const classes = useStyles();
 
+  const handleAddToCart = () => onAddToCart(product.id, 1);
+
   return (
-    <div>
-      <Card className={classes.root}>
-        <CardMedia
-          className={classes.media}
-          image={product.image}
-          title={product.name}
-        />
-        <CardContent>
-          <div className={classes.cardContent}>
-            <Typography variant='h5' gutterBottom>
-              {product.name}
-            </Typography>
-            <Typography variant='h5'>{product.price}</Typography>
-          </div>
-          <Typography variant='body2' color='textSecondary'>
-            {product.description}
+    <Card className={classes.root}>
+      <CardMedia
+        className={classes.media}
+        image={product.media.source}
+        title={product.name}
+      />
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography gutterBottom variant='h5' component='h2'>
+            {product.name}
           </Typography>
-        </CardContent>
-        <CardActions disableSpacing className={classes.cardActions}>
-          <IconButton aria-label='Add to Cart'>
-            <AddShoppingCart />
-          </IconButton>
-        </CardActions>
-      </Card>
-    </div>
+          <Typography gutterBottom variant='h5' component='h2'>
+            ${product.price.formatted}
+          </Typography>
+        </div>
+        <Typography
+          dangerouslySetInnerHTML={{ __html: product.description }}
+          variant='body2'
+          color='textSecondary'
+          component='p'
+        />
+      </CardContent>
+      <CardActions disableSpacing className={classes.cardActions}>
+        <IconButton aria-label='Add to Cart' onClick={handleAddToCart}>
+          <AddShoppingCart />
+        </IconButton>
+      </CardActions>
+    </Card>
   );
 };
 
